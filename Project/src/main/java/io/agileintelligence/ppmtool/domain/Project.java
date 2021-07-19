db1fcffe28893de96fdbbb45f2f0bd6edea4a1d2
JJJYXT <jiangjinjinyxt@gmail.com>
2021-07-19 13:54:09 -0500

    Project Object

---
+++ Project/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -0,0 +1,12 @@
+package io.agileintelligence.ppmtool.domain;
+
+/**
+ * Project
+ *
+ * @author JJJ
+ * @date 7/19/21 1:53 PM
+ * @email jiangjinjinyxt@gmail.com
+ * @since 1.0.0
+ */
+public class Project {
+}
